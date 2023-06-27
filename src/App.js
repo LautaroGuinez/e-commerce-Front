5a0a05517dd796629d892770503788b77765f7dd
LautaroGuinez <laucha_09@live.com>
2023-06-27 16:40:28 -0300

    add common  "register"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Button } from "@mui/material";
 import { Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
 
 import Register from "./common/register";
 
 function App() {
   return (
     <div>
-      <Button variant="contained" color="primary">
-        Hola, Material-UI
-      </Button>
+      <Link to="/register">
+        <Button variant="contained" color="primary">
+          Hola, Material-UI
+        </Button>
+      </Link>
       <Routes>
         <Route path="/register" element={<Register />} />
       </Routes>
