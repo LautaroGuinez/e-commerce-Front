68bca0e7ad4d94dfbab5e07f7f23b287fe7765d1
LautaroGuinez <laucha_09@live.com>
2023-06-27 12:26:23 -0300

    testeando push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "@mui/material";
+import { Route, Routes } from "react-router";
+
+import Register from "./common/register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="contained" color="primary">
+        Hola, Material-UI
+      </Button>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
