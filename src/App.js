bfff73c26884e69e546bfe6410c595754bb8c728
v4leng <valem.g_13@hotmail.com>
2023-06-28 17:52:11 -0300

    component-home

--- src/App.js
+++ src/App.js
@@ -1,50 +1,21 @@
 import React from "react";
-import { Button } from "@mui/material";
 import { Route, Routes } from "react-router";
-import { Link } from "react-router-dom";
-
+import Home from "./components/home";
 import ProductsCards from "./common/productsCards";
-import fakeData from "./utils/fakeData";
 import Register from "./common/register";
 import Login from "./common/login";
-import Cars from "./common/cars"
+import Cars from "./common/cars";
 import Navbar from "./components/Navbar";
 
 function App() {
- 
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "80vh",
-              }}
-            >
-              <Link to="/login">
-                <Button variant="contained" color="primary">
-                  Login
-                </Button>
-              </Link>
-              <Link to="/product">
-                <Button variant="contained" color="primary">
-                  Registro
-                </Button>
-              </Link>
-            </div>
-          }
-        ></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/product" element={<ProductsCards />} />
-        <Route path="/Cars" element={<Cars />}/>
-
+        <Route path="/Cars" element={<Cars />} />
       </Routes>
     </>
   );
