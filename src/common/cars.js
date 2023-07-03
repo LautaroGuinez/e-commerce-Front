762e7f3327519af259ee214a119ba5f02298d2ec
fggutierrez98 <fggutierrez98@gmail.com>
2023-07-03 15:48:27 -0300

    change to english

--- src/common/cars.js
+++ src/common/cars.js
@@ -13,8 +13,6 @@ import {
 } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
 
 const Cars = () => {
   // prueba con fake date
