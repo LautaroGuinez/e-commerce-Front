61ef219817481d629fb3137446213fff5e975862
v4leng <valem.g_13@hotmail.com>
2023-07-03 18:15:10 -0300

    store-cars-persistence

--- src/state/store.js
+++ src/state/store.js
@@ -3,11 +3,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import reducerUser from "./user";
 import carsReducer from "./cars";
 
+const persistenceState = localStorage.getItem("reduxState")
+  ? JSON.parse(localStorage.getItem("reduxState"))
+  : {};
+
 const store = configureStore({
   reducer: {
     user: reducerUser,
     cars: carsReducer,
   },
+  preloadedState: persistenceState,
+});
+
+store.subscribe(() => {
+  const state = store.getState();
+  localStorage.setItem("reduxState", JSON.stringify(state));
 });
 
 export default store;
